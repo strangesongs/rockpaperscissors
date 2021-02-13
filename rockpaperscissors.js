cef8702becd7847678788aadc57485024df5b140
Josh Cretella <jcrtll@protonmail.com>
2021-02-13 12:15:42 -0800

    Adding initial JS file and comments

---
+++ rockpaperscissors.js
@@ -0,0 +1,18 @@
+function computerPlay 
+
+// should randomly choose from rock, paper or scissors as the computer guess
+
+function playRound 
+
+// should take two parameters -- computerPLay and computerSelection -- and then return a string with the winner and loser of each game
+
+if userGuess == guess compare to computerGuess and determine result
+
+function Game will use previous function (playRound) to keep score of each round and then return the result and update the score
+
+// i'll need the following variables:
+userGuess (which will be inputted by user via prompt)
+computerGuess (which will be randomly chosen)
+gameScore
+guesses 
+
